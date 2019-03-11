9979a735d7b9c2e1d3984eaf006efb096ebee2e4
Matt Sutton <mattsuttondev@gmail.com>
2019-03-11 15:10:25 -0400

    Initial Commit

---
+++ News/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  News
+//
+//  Created by Matthew Sutton on 3/11/19.
+//  Copyright © 2019 Matthew Sutton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
