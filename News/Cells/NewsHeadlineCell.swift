223bd3631443b1e55dad54c6b527a5b6986d5730
Matt Sutton <mattsuttondev@gmail.com>
2019-03-11 16:13:35 -0400

    Added cell content

--- News/Cells/NewsHeadlineCell.swift
+++ News/Cells/NewsHeadlineCell.swift
@@ -6,4 +6,19 @@
 //  Copyright © 2019 Matthew Sutton. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class NewsHeadlineCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLbl: UILabel!
+    @IBOutlet weak var descriptionLbl: UILabel!
+    @IBOutlet weak var headlineImgView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        headlineImgView.layer.cornerRadius = 8
+        headlineImgView.layer.masksToBounds = true
+    }
+    
+}
--- News/Managers/ThemeManager.swift
+++ News/Managers/ThemeManager.swift
@@ -6,4 +6,17 @@
 //  Copyright © 2019 Matthew Sutton. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class ThemeManager {
+    
+    static func setup() {
+        
+        UINavigationBar.appearance().barTintColor = UIColor(red: 0.188, green: 0.243, blue: 0.306, alpha: 1.00)
+        UINavigationBar.appearance().titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        UINavigationBar.appearance().largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        
+        
+    }
+    
+}
